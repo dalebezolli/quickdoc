2c4a1b02fd7e60f8fe0cb1d397098c140080dd2f
Dale Bezolli <dalebezolli@gmail.com>
2023-11-17 03:11:19 +0200

    Create custom iframe-like display for linkviewer

--- content/about.md
+++ content/about.md
@@ -1,2 +1,14 @@
 # A fun little about page
-You know...
+Let's test that this works properly.
+
+A very important list about things I own:
+- ~~House~~
+- Nice Laptop
+- Phone
+	- Probably need a better one though
+- D&D Dice
+	1. In dire need for more though
+	1. Do you think that numbers are cool?
+- Myself
+- Imagination
+
--- static/test.js
+++ static/test.js
@@ -1,7 +1,12 @@
 'use strict';
-const PAGEVIEW_TOGGLE_TIME_MS = 1000;
+const PAGEVIEW_SHOW_TIME_MS = 250;
+const PAGEVIEW_HIDE_TIME_MS = 150;
 const LINKVIEWER_ID = 'linkviewer';
 const LINKVIEWER_HIDDEN_CSS = 'js-linkviewer--hidden';
+
+const linkViewer = document.getElementById(LINKVIEWER_ID);
+let displayReference = null;
+let hideReference = null;
 
 if(isALinkViewer()) {
 	document.body.style.backgroundColor = '#ff0bbb';
@@ -18,68 +23,141 @@ function isALinkViewer() {
 
 function setupLinkViewerHoverPopupEventHandlers() {
 	for(const link of document.links) {
-		const linkViewer = document.getElementById(LINKVIEWER_ID);
-
-		let displayReference = null;
-		let hideReference = null;
 
 		link.addEventListener('mouseenter', event => {
-			const debugString = `LINK: Attempting to SHOW with ${link} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
-			console.info(debugString);
-			onPrepareToToggleLinkViewerFrame(true, event.target.href, event.clientX, event.clientY);
+			const debugString = `LINK: Attempting to SHOW with ${event.target.href} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
+			console.debug(debugString);
+
+			const boundingBox = event.target.getBoundingClientRect();
+			onPrepareToToggleLinkViewerFrame(true, event.target.href, boundingBox.bottom, boundingBox.right);
 		});
+
 		link.addEventListener('mouseleave', event => {
-			const debugString = `LINK: Attempting to HIDE with ${link} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
-			console.info(debugString);
-			onPrepareToToggleLinkViewerFrame(false, event.target.href, event.clientX, event.clientY);
+			const debugString = `LINK: Attempting to HIDE with ${event.target.href} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
+			console.debug(debugString);
+
+			const boundingBox = event.target.getBoundingClientRect();
+			onPrepareToToggleLinkViewerFrame(false, event.target.href, boundingBox.bottom, boundingBox.rigth);
 		});
 
-		linkViewer.addEventListener('mouseenter', event => {
-			const debugString = `LINKVIEWER: Attempting to SHOW with ${link} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
-			console.info(debugString);
-			onPrepareToToggleLinkViewerFrame(true, event.target.src, event.clientX, event.clientY);
-		});
-		linkViewer.addEventListener('mouseleave', event => {
-			const debugString = `LINKVIEWER: Attempting to HIDE with ${link} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
-			console.info(debugString);
-			onPrepareToToggleLinkViewerFrame(false, event.target.src, event.clientX, event.clientY);
-		});
+	}
 
-		function onPrepareToToggleLinkViewerFrame(shouldShow, link, mouseX, mouseY) {
-			const oppositeReference = shouldShow ? hideReference : displayReference;
-			clearTimeout(oppositeReference);
+	linkViewer.addEventListener('mouseenter', event => {
+		const debugString = `LINKVIEWER: Attempting to SHOW with ${event.target.src} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
+		console.debug(debugString);
 
-			const linkViewer = document.getElementById(LINKVIEWER_ID);
-			const isCurrentlyShown = !linkViewer.classList.contains(LINKVIEWER_HIDDEN_CSS);
-			const noNeedToToggle = shouldShow ? isCurrentlyShown : !isCurrentlyShown;
+		const boundingBox = event.target.getBoundingClientRect();
+		onPrepareToToggleLinkViewerFrame(true, event.target.src, boundingBox.bottom, boundingBox.right);
+	});
 
-			if(noNeedToToggle) return;
+	linkViewer.addEventListener('mouseleave', event => {
+		const debugString = `LINKVIEWER: Attempting to HIDE with ${event.target.src} (displayReference: ${displayReference}, hideReference: ${hideReference})`;
+		console.debug(debugString);
 
-			const toggleReference = setTimeout(_ => {
-					toggleLinkViewerFrame(shouldShow, link, mouseX, mouseY);
-				},
-				PAGEVIEW_TOGGLE_TIME_MS
-			);
+		const boundingBox = event.target.getBoundingClientRect();
+		onPrepareToToggleLinkViewerFrame(false, event.target.src, boundingBox.bottom, boundingBox.right);
+	});
+}
 
-			if(shouldShow) {
-				displayReference = toggleReference;
-			} else {
-				hideReference = toggleReference;
-			}
-		}
+/**
+ * Handles timers for display & correctly positioning the link viewer
+ */
+function onPrepareToToggleLinkViewerFrame(shouldShow, link, mouseX, mouseY) {
+	shouldShow &&  console.debug(`Setting the display position: (${mouseX},${mouseY})`);
+	const oppositeReference = shouldShow ? hideReference : displayReference;
+	clearTimeout(oppositeReference);
+
+	const linkViewer = document.getElementById(LINKVIEWER_ID);
+	const isCurrentlyShown = !linkViewer.classList.contains(LINKVIEWER_HIDDEN_CSS);
+	const noNeedToToggle = shouldShow ? isCurrentlyShown : !isCurrentlyShown;
+
+	if(noNeedToToggle) return;
+
+	const [displayX, displayY] = calculateTopLeftDisplayPosition(mouseX, mouseY);
+	const toggleReference = setTimeout(_ => {
+			toggleLinkViewerFrame(shouldShow, link, displayX, displayY);
+		},
+		shouldShow ? PAGEVIEW_SHOW_TIME_MS : PAGEVIEW_HIDE_TIME_MS
+	);
+
+	if(shouldShow) {
+		displayReference = toggleReference;
+	} else {
+		hideReference = toggleReference;
 	}
 }
 
+function calculateTopLeftDisplayPosition(mouseX, mouseY) {
+	return [mouseX, mouseY];
+}
+
+
+/**
+ * Sets the data and toggles the link viewer
+ */
 function toggleLinkViewerFrame(isEnabled, link, mouseX, mouseY) {
 	const linkViewer = document.getElementById(LINKVIEWER_ID);
-	
+	linkViewer.style = `--pos-x: ${mouseX}px; --pos-y: ${mouseY}px`;
 
-	console.info(`${isEnabled ? 'SHOW' : 'HIDE'}: ${link}`);
+	console.debug(`${isEnabled ? 'SHOW' : 'HIDE'}: ${link}`);
 	if(isEnabled) {
-		linkViewer.classList.remove(LINKVIEWER_HIDDEN_CSS);
-		linkViewer.src = link;
+		constructLinkViewerData(link).then(divLinkViewerData => {
+			while(linkViewer.firstChild) {
+				linkViewer.firstChild.remove();
+			}
+			linkViewer.appendChild(divLinkViewerData);
+
+			linkViewer.classList.remove(LINKVIEWER_HIDDEN_CSS);
+		});
+
 	} else {
 		linkViewer.classList.add(LINKVIEWER_HIDDEN_CSS);
 		return;
 	}
+}
+
+async function constructLinkViewerData(link) {
+	const url = new URL(link);
+
+	const htmlDataRequest = await fetch(url);
+	const htmlData = await htmlDataRequest.text();
+	const mimeType = htmlDataRequest.headers.get('content-type')
+		.slice(0, htmlDataRequest.headers.get('content-type').search(';'));
+
+	const htmlParser = new DOMParser();
+	const noteDocument = htmlParser.parseFromString(htmlData, mimeType);
+
+	const divLinkViewerData = document.createElement('DIV');
+	divLinkViewerData.classList.add('linkviewer__data');
+
+	if(!url.hash) {
+		divLinkViewerData.innerHTML = noteDocument.getElementById('note').innerHTML;
+	} else {
+		const calculatedHeadingIDFromHash = decodeURIComponent(url.hash).toLowerCase().replaceAll(' ', '-').slice(1);
+		const noteRoot = noteDocument.querySelector('#note');
+
+		let childIndex = 0;
+		let foundStartNode = false;
+		while(childIndex < noteRoot.children.length && !foundStartNode) {
+			if(noteRoot.children[childIndex].id === calculatedHeadingIDFromHash) {
+				foundStartNode = true;
+			} else {
+				childIndex++;
+			}
+		}
+
+		const startNodeTagName = noteRoot.children[childIndex].tagName;
+		divLinkViewerData.append(noteRoot.children[childIndex++].cloneNode(true));
+		while(
+			childIndex < noteRoot.children.length && 
+			noteRoot.children[childIndex] && 
+			noteRoot.children[childIndex].tagName !== startNodeTagName
+		) {
+
+			divLinkViewerData.append(noteRoot.children[childIndex++].cloneNode(true));
+		}
+
+	}
+
+	return divLinkViewerData;
 }
