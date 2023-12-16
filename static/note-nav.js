678a8ef04bccb77e6330dd6ecb61a8a40c7cafec
Dale Bezolli <dalebezolli@gmail.com>
2023-12-16 14:11:38 +0200

    Correctly setup scrolling from table of contents

--- static/note-nav.js
+++ static/note-nav.js
@@ -1,44 +1,55 @@
 const NOTE_ROOT = 'note';
-const NOTE_HEADINGS_LEVEL = 'H2';
 const NOTE_NAVIGATION_LIST_ROOT_ID = 'toc';
 const NOTE_NAVIGATION_LIST_ITEM_CLASS = 'toc-item';
 const NOTE_NAVIGATION_LIST_LINK_CLASS = 'toc-link';
 
-const noteNavListRoot = document.getElementById(NOTE_NAVIGATION_LIST_ROOT_ID);
-const noteWalker = document.createTreeWalker(document.getElementById(NOTE_ROOT), NodeFilter.SHOW_ELEMENT);
+const NOTE_HEADING = 'H2';
 
-fillNoteNavigation(noteNavListRoot, noteWalker);
+noteSetup();
 
-function fillNoteNavigation(noteNavigationRoot, noteWalker) {
-	if(!noteNavigationRoot || !noteWalker) return;
+function noteSetup() {
+	const noteNavListRoot = document.getElementById(NOTE_NAVIGATION_LIST_ROOT_ID);
 
+	const noteWalker = document.createTreeWalker(document.getElementById(NOTE_ROOT), NodeFilter.SHOW_ELEMENT);
 	while(noteWalker.nextNode()) {
-		if(noteWalker.currentNode.tagName === 'PRE') {
-			noteWalker.currentNode.parentNode.style.position = 'relative';
-
-			const buttonCopy = document.createElement('BUTTON');
-			buttonCopy.textContent = 'Copy';
-			buttonCopy.classList.add('button-secondary');
-			noteWalker.currentNode.parentNode.appendChild(buttonCopy);
-
-			buttonCopy.addEventListener('click', event => {
-				const code = event.currentTarget.parentNode.querySelector('pre').textContent;
-				navigator.clipboard.writeText(code);
-			});
+		switch(noteWalker.currentNode.tagName) {
+			case NOTE_HEADING:
+				setupHeadingNavigation(noteNavListRoot, noteWalker.currentNode);
+				break;
+			case 'PRE':
+				setupCodeBlocks(node);
+				break;
 		}
+	}
+}
 
-		if(noteWalker.currentNode.tagName != NOTE_HEADINGS_LEVEL) continue;
+function setupCodeBlocks(node) {
+	node.parentNode.style.position = 'relative';
 
+	const buttonCopy = document.createElement('BUTTON');
+	buttonCopy.textContent = 'Copy';
+	buttonCopy.classList.add('button-secondary');
+	node.parentNode.appendChild(buttonCopy);
+
+	buttonCopy.addEventListener('click', event => {
+		const code = event.currentTarget.parentNode.querySelector('pre').textContent;
+		navigator.clipboard.writeText(code);
+	});
+}
+
+function setupHeadingNavigation(noteNavigationRoot, node) {
 		const navigationListItem = document.createElement('LI');
 		navigationListItem.classList.add(NOTE_NAVIGATION_LIST_ITEM_CLASS);
 
-		const navigationAnchor = document.createElement('A');
+		const navigationAnchor = document.createElement('BUTTON');
 		navigationAnchor.classList.add(NOTE_NAVIGATION_LIST_LINK_CLASS);
-		navigationAnchor.href = `#${noteWalker.currentNode.id}`;
-		navigationAnchor.textContent = noteWalker.currentNode.textContent;
+		navigationAnchor.addEventListener('click', event => {
+			const domHeading = document.getElementById(node.id);
+			const absoluteElementPositionWithoutPadding = domHeading.getBoundingClientRect().top - domHeading.getBoundingClientRect().height - 16;
+			window.scrollTo({top: absoluteElementPositionWithoutPadding + window.pageYOffset});
+		});
+		navigationAnchor.textContent = node.textContent;
 
 		noteNavigationRoot.append(navigationListItem);
 		navigationListItem.append(navigationAnchor);
-		console.log(noteWalker.currentNode);
-	}
 }
