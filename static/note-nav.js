b87edf2aa4f079e53ef51053931046c93653b9e4
Dale Bezolli <dalebezolli@gmail.com>
2023-12-17 15:36:59 +0200

    Highlight toc's corresponding heading

--- static/note-nav.js
+++ static/note-nav.js
@@ -1,10 +1,18 @@
+'use strict';
+
 const NOTE_ROOT = 'note';
 const NOTE_NAVIGATION_LIST_ROOT_ID = 'toc';
 const NOTE_NAVIGATION_LIST_ITEM_CLASS = 'toc-item';
 const NOTE_NAVIGATION_LIST_LINK_CLASS = 'toc-link';
 
 const NOTE_HEADING = 'H2';
+let previousHighlightID = '';
 
+const observer = new IntersectionObserver(highlightNavigation, {
+	root: null,
+	rootMargin: '-50px 0px -60% 0px',
+	threshold: 1.0
+});
 noteSetup();
 
 function noteSetup() {
@@ -37,19 +45,37 @@ function setupCodeBlocks(node) {
 	});
 }
 
-function setupHeadingNavigation(noteNavigationRoot, node) {
-		const navigationListItem = document.createElement('LI');
-		navigationListItem.classList.add(NOTE_NAVIGATION_LIST_ITEM_CLASS);
+function setupHeadingNavigation(domNoteNavigationRoot, domNode) {
+	const domNavigationListItem = document.createElement('LI');
+	domNavigationListItem.id = 'toc-' + domNode.id;
+	domNavigationListItem.classList.add(NOTE_NAVIGATION_LIST_ITEM_CLASS);
 
-		const navigationAnchor = document.createElement('BUTTON');
-		navigationAnchor.classList.add(NOTE_NAVIGATION_LIST_LINK_CLASS);
-		navigationAnchor.addEventListener('click', event => {
-			const domHeading = document.getElementById(node.id);
-			const absoluteElementPositionWithoutPadding = domHeading.getBoundingClientRect().top - domHeading.getBoundingClientRect().height - 16;
-			window.scrollTo({top: absoluteElementPositionWithoutPadding + window.pageYOffset});
-		});
-		navigationAnchor.textContent = node.textContent;
+	const domNavigationAnchor = document.createElement('BUTTON');
+	domNavigationAnchor.classList.add(NOTE_NAVIGATION_LIST_LINK_CLASS);
+	domNavigationAnchor.addEventListener('click', event => {
+		const domHeading = document.getElementById(domNode.id);
+		const absoluteElementPositionWithoutPadding = domHeading.getBoundingClientRect().top - domHeading.getBoundingClientRect().height - 16;
+		window.scrollTo({top: absoluteElementPositionWithoutPadding + window.pageYOffset});
+	});
+	domNavigationAnchor.textContent = domNode.textContent;
 
-		noteNavigationRoot.append(navigationListItem);
-		navigationListItem.append(navigationAnchor);
+	domNoteNavigationRoot.append(domNavigationListItem);
+	domNavigationListItem.append(domNavigationAnchor);
+	
+	if(observer !== null) observer.observe(domNode);
+}
+
+function highlightNavigation(entries) {
+	for(const entry of entries) {
+		if(!entry.isIntersecting) continue;
+		const domTarget = entry.target;
+		const domTocNav = document.getElementById(`toc-${domTarget.id}`);
+		const domPrevTocNav = document.getElementById(`toc-${previousHighlightID}`);
+
+		if(domPrevTocNav) domPrevTocNav.classList.remove('js-toc-link--highlight');
+		domTocNav.classList.add('js-toc-link--highlight');
+		previousHighlightID = domTarget.id;
+		return;
+	}
+
 }
